3a725da3a15caf5c6d2956b4af7c643fdefc451c
Prompiriya1 <132637448+Prompiriya1@users.noreply.github.com>
2023-06-22 11:51:59 +0000

    Initial commit

---
+++ node.h
@@ -0,0 +1,39 @@
+class node {
+private:
+  int id;
+  string name;
+  node *next;
+
+public:
+  node(int, string);
+  void print();
+  node *get_next();
+  void set_next(node *);
+  ~node(){
+    cout<<name<<"delete"<<endl;
+  }
+};
+
+typedef node* nodePtr;
+//node x;
+
+typedef node *nodePtr;
+nodePtr y;
+
+node::node(int i,string n){
+  id=i;
+  name=n;
+  next=NULL;
+}
+
+void node::print(){
+  cout<<"ID: "<<id<<" Name: "<<name<<endl;
+}
+
+nodePtr node::get_next(){
+    return next;
+}
+
+void node::set_next(nodePtr x){
+  next=x;
+}
